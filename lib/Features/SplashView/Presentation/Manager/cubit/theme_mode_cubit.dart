b689991588d584ee5439e9308b63433edfa1a1cd
Zeyad Eldesouki <zeyadeldesouki@gmail.com>
2025-02-25 16:50:34 +0200

    changethememode

--- lib/Features/SplashView/Presentation/Manager/cubit/theme_mode_cubit.dart
+++ lib/Features/SplashView/Presentation/Manager/cubit/theme_mode_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 
 class ThemeModeCubit extends Cubit<ThemeMode> {
   ThemeModeCubit() : super(ThemeMode.light);
